bdb8da534b94ac5a93e0211b2860997f599f9bb5
Alex <alex.alexandrov.1988@gmail.com>
2023-12-31 17:06:45 +0300

    counterRedux

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -2,35 +2,50 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import Button from "./Button";
 import SetCounter from "./SetCounter";
+import {useDispatch, useSelector} from "react-redux";
+import {CountType, incrementAC, resetHandlerAC} from "./counter-reducer";
+import {AppRootStateType} from "./store";
+
+type stateType ={
+    initialState: number
+}
 
 function AppWithRedux() {
 
-    const [count, setCount] = useState(0)
-    const [maxValue, setMaxValue] = useState(5)
+
+
+    // const [count, setCount] = useState(0)
+    // const [maxValue, setMaxValue] = useState(5)
     const [reset,setReset]= useState(true)
 
+const count = useSelector<AppRootStateType,CountType>((state)=>state.counter);
+
+    const dispatch = useDispatch();
     const resetsCounter=()=>{
         setReset(!reset)
     }
 
     const LocalStorage = (start: number, max: number) => {
-        setCount(start)
-        setMaxValue(max)
+        // setCount(start)
+        // setMaxValue(max)
         setReset(!reset)
     }
 
 
     const IncrementHandler = () => {
-        setCount(count + 1)
+        const action =incrementAC()
+        dispatch(action)
+
     }
 
 
     const ResetHandler = () => {
-        setCount(0)
+        const action = resetHandlerAC()
+        dispatch(action)
     }
 
-    const countNon = count === 0;
-    const maxCount = count >= maxValue;
+    const countNon = count.count === 0;
+    const maxCount = count.count >= count.maxValue;
 
 
     return (
@@ -40,8 +55,8 @@ function AppWithRedux() {
                 <SetCounter  LocalStorage={LocalStorage}/>
                 : <div className="App">
 
-                <span className={count >= maxValue ? 'red' : ''}>
-             {count === maxValue ? 'Wrong' : count}</span>
+                <span className={count.count >= count.maxValue ? 'red' : ''}>
+             {count.count === count.maxValue ? 'Wrong' : count.count}</span>
                     <div className='btn'>
                         <Button onClick={IncrementHandler} disabled={maxCount} name={'inc'}/>
                         <Button onClick={ResetHandler} disabled={countNon} name={'reset'}/>
--- src/counter-reducer.ts
+++ src/counter-reducer.ts
@@ -1,25 +1,45 @@
+export type IncrementType = {
+    type: 'INCREMENT',
 
-export type AddTaskActionType = {
-    type: 'COUNT-PLUS',
-    todolistId: string
-    title: string
+}
+export type ResetHandlerType ={
+  type:  'RESET'
 }
 
-type CountType ={
-    initialState:number
+
+type ActionsType = IncrementType |ResetHandlerType
+
+export type CountType = {
+    count: number
+    maxValue:number
 }
 
-let initialState= 0
+let initialState:CountType ={
+    count:0,
+    maxValue:5
+};
 
 
-export const counterReducer =(state:CountType=initialState,action)=>{
-    switch (action.type){
-        case 'COUNT-PLUS':{
-            return;
+export const counterReducer = (state = initialState, action: ActionsType):CountType => {
+    switch (action.type) {
+        case 'INCREMENT':{
+            return {...state, count:state.count +=1 };
         }
-        case 'ADD-TOSTORAGE':{
-            return
+        case 'RESET' :{
+return {...state,count:0}
         }
+            default: return state;
+
     }
-    return state
+
+
+};
+
+export const incrementAC = (): IncrementType => {
+    return {type: 'INCREMENT'}as const
+}
+
+
+export const resetHandlerAC =():ResetHandlerType =>{
+    return {type:'RESET'}as const
 }
